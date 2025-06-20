47f0181d536d2034d7da1667bbff9c228274262a
VitorV1torV170R <vitor.oliveiracosta94@gmail.com>
2025-06-20 18:44:43 -0300

    Ajuste criação de usuario 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: [:new, :create]
+  allow_unauthenticated_access only: [:new, :create]
 
   def new
     @user = User.new
